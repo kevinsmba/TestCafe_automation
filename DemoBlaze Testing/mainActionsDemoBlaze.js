b546bbbef1abc10f4f19d5c6dc7694d1605ef637
kevinsmba <kevinsmba@gmail.com>
2023-08-16 16:42:08 -0500

    new test case added

--- DemoBlaze Testing/mainActionsDemoBlaze.js
+++ DemoBlaze Testing/mainActionsDemoBlaze.js
@@ -8,4 +8,18 @@ export default class MainActionsDemoBlaze {
     async clickSignUp() {
         await t.click(mainSelectors.signUp);
     }
+    async TypeUsername(username){
+        await t.typeText(mainSelectors.typeUsername, username);
+    }
+    async TypePassword(password){
+        await t.typeText(mainSelectors.typePassword, password);
+    }
+    async clickSubmitAccountTrue(){
+        await t.setNativeDialogHandler(() => true);
+        await t.click(mainSelectors.submitAccount);
+    }
+    async clickSubmitAccountFalse(){
+        await t.setNativeDialogHandler(() => false);
+        await t.click(mainSelectors.submitAccount);
+    }
 }
--- DemoBlaze Testing/mainSignUp.js
+++ DemoBlaze Testing/mainSignUp.js
@@ -1,17 +1,22 @@
 import MainActionsDemoBlaze from "./mainActionsDemoBlaze.js";
 import { t } from 'testcafe';
 
+
 fixture('Complete the sign up for a new customer')
     .page('https://demoblaze.com/');
 
 test('Complete the sign up process', async t => {
     const mainActionsD = new MainActionsDemoBlaze();
     await mainActionsD.clickSignUp();
+    await mainActionsD.TypeUsername('Kevin Torres');
+    await mainActionsD.TypePassword('1234Jkln');
+    await mainActionsD.clickSubmitAccountTrue();
 });
 
-/*test('Verify the sign up confirmation', async t => {
-    const mainActions = new MainActions();
-    await mainActions.typeName('John Doe');
-    await mainActions.submitName();
-    await t.expect(mainActions.articleHeader().innerText).eql('Thank you, John Doe!');git 
-});*/
+test('Verify that the account cannot be created again', async t => {
+    const mainActionsD = new MainActionsDemoBlaze();
+    await mainActionsD.clickSignUp();
+    await mainActionsD.TypeUsername('Kevin Torres');
+    await mainActionsD.TypePassword('1234Jkln');
+    await mainActionsD.clickSubmitAccountFalse();
+});
